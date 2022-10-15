c33b8b22b1a7815b1b5622236f1a37cb8e303c62
dev-DTECH <deep64222@gmail.com>
2022-10-15 13:29:45 +0530

    15-oct

---
+++ problems/B_Find_Marble.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <limits>
+#include <vector>
+#include <bitset>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <time.h>
+using namespace std;
+#define MOD 1000000007LL
+#define LL long long
+#define ULL unsigned long long
+#define LD long double
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+#define si(n) scanf("%d", &n)
+#define sf(n) scanf("%f", &n)
+#define sl(n) scanf("%lld", &n)
+#define slu(n) scanf("%llu", &n)
+#define sd(n) scanf("%lf", &n)
+#define ss(n) scanf("%s", n)
+#define pnl printf("\n")
+#define REP(i, n) for (int i = 0; i < (n); i++)
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define FORR(i, n) for (int i = (n); i >= 0; i--)
+#define DB(x) cout << "\n" \
+                   << #x << " = " << (x) << "\n";
+#define CL(a, b) memset(a, b, sizeof(a))
+#define GOLD ((1 + sqrt(5)) / 2)
+const double PI = 3.14159265358979323846264338327950288419716939937510582097494459230;
+void swaps(char *x, char *y)
+{
+    char temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+void swapi(int *a, int *b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+ULL gcd(ULL a, ULL b)
+{
+    if (a == 0)
+        return b;
+    if (b == 0)
+        return a;
+    if (a == 1 || b == 1)
+        return 1;
+    if (a == b)
+        return a;
+    if (a > b)
+        return gcd(b, a % b);
+    else
+        return gcd(a, b % a);
+}
+#define SIZE 1000001
+void preprocess()
+{
+} // end prepreprocess
+void refresh()
+{
+} // end refresh
+void compute()
+{
+    int n,s,t;
+    cin >>n>>s>>t;
+    int ar[n];
+    set<int> st;
+    FOR(i,0,n){
+        cin >>ar[i];
+    }
+    int p=s-1;
+    int c=0;
+    while(p!=t-1){
+        if(st.find(ar[p])!=st.end())
+        {
+            cout << -1;
+            return;
+        }  
+        st.insert(ar[p]);
+        p=ar[p]-1;
+        c++;
+    }
+    cout << c;
+} // end compute
+int main()
+{
+#ifdef debug
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("log.txt", "w", stderr);
+#endif
+    int t, i, j;
+    preprocess();
+    // scanf("%d", &t);
+    t = 1;
+    while (t--)
+    {
+        compute();
+    } // end while
+#ifdef debug
+    fprintf(stdout, "\nTIME: %.3lf sec\n", (double)clock() / (CLOCKS_PER_SEC));
+#endif
+    return 0;
+} // end main*
---
+++ problems/B_Little_Pony_and_Sort_by_Shift.cpp
@@ -0,0 +1,131 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <limits>
+#include <vector>
+#include <bitset>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <time.h>
+using namespace std;
+#define MOD 1000000007LL
+#define LL long long
+#define ULL unsigned long long
+#define LD long double
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+#define si(n) scanf("%d", &n)
+#define sf(n) scanf("%f", &n)
+#define sl(n) scanf("%lld", &n)
+#define slu(n) scanf("%llu", &n)
+#define sd(n) scanf("%lf", &n)
+#define ss(n) scanf("%s", n)
+#define pnl printf("\n")
+#define REP(i, n) for (long long int i = 0; i < (n); i++)
+#define FOR(i, a, b) for (long long int i = (a); i < (b); i++)
+#define FORR(i, n) for (long long int i = (n); i >= 0; i--)
+#define DB(x) cout << "\n" \
+                   << #x << " = " << (x) << "\n";
+#define CL(a, b) memset(a, b, sizeof(a))
+#define GOLD ((1 + sqrt(5)) / 2)
+const double PI = 3.14159265358979323846264338327950288419716939937510582097494459230;
+void swaps(char *x, char *y)
+{
+    char temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+void swapi(int *a, int *b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+ULL gcd(ULL a, ULL b)
+{
+    if (a == 0)
+        return b;
+    if (b == 0)
+        return a;
+    if (a == 1 || b == 1)
+        return 1;
+    if (a == b)
+        return a;
+    if (a > b)
+        return gcd(b, a % b);
+    else
+        return gcd(a, b % a);
+}
+#define SIZE 1000001
+void preprocess()
+{
+} // end prepreprocess
+void refresh()
+{
+} // end refresh
+void compute()
+{
+    LL int n;
+    cin >> n;
+    vector<LL int> ar(n);
+    LL int m = INT_MIN, mi = 0;
+    FOR(i, 0, n)
+    {
+        cin >> ar[i];
+        // if(m<=ar[i]){
+        //     m=ar[i];
+        //     mi=i;
+        // }
+    }
+    int f = 1;
+    FOR(i, 0, n - 1)
+    {
+        if (ar[i] > ar[i + 1])
+        {
+            mi = i;
+            f = 0;
+        }
+    }
+    if (f)
+    {
+        cout << 0;
+        return;
+    }
+    LL int p = (mi + 1) % n;
+    FOR(i, 0, n - 1)
+    {
+        if (ar[p] > ar[(p + 1) % n])
+        {
+            // cout << -1 << ":" << p;
+            cout << -1;
+            return;
+        }
+        p = (p + 1) % n;
+    }
+    cout << n - 1 - mi;
+} // end compute
+int main()
+{
+#ifdef debug
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("log.txt", "w", stderr);
+#endif
+    int t, i, j;
+    preprocess();
+    // scanf("%d", &t);
+    t = 1;
+    while (t--)
+    {
+        compute();
+    } // end while
+#ifdef debug
+    fprintf(stdout, "\nTIME: %.3lf sec\n", (double)clock() / (CLOCKS_PER_SEC));
+#endif
+    return 0;
+} // end main*
